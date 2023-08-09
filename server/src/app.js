f35d358f00dcff8389175609c8dd2727d207d23c
Lioz Akirav <lioz.akirav@gmail.com>
2023-08-09 03:24:37 +0300

    add basic files

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const { connect } = require('./config/database')
+const logger = require('./config/logger')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Middleware
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Connect to the database
+connect(process.env.MONGODB_URI)
+
+// Routes
+
+// Start the server
+const server = app.listen(port, () => {
+  logger.info(`Server started on http://localhost:${port}`)
+})
+
+module.exports = { app, server }
---
+++ server/src/config/database.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+const logger = require('./logger')
+
+let isConnected = false
+let connection = null
+
+async function connect (connectionString) {
+  try {
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    isConnected = true
+    connection = mongoose.connection
+    logger.info(`Connected to MongoDB: ${connectionString}`)
+  } catch (error) {
+    logger.error('Failed to connect to MongoDB:', error)
+    throw error
+  }
+}
+
+async function disconnect () {
+  if (!isConnected) {
+    logger.error('No active MongoDB connection to disconnect from')
+    return
+  }
+
+  try {
+    await connection.close()
+    isConnected = false
+    connection = null
+    logger.info('Disconnected from MongoDB')
+  } catch (error) {
+    logger.error('Failed to disconnect from MongoDB:', error)
+    throw error
+  }
+}
+
+async function cleanupDatabase () {
+  if (!isConnected) {
+    logger.error('No active MongoDB connection to clean up')
+    return
+  }
+
+  try {
+    await connection.dropDatabase()
+    logger.info('Database cleaned up')
+  } catch (error) {
+    logger.error('Failed to clean up database:', error)
+    throw error
+  }
+}
+
+module.exports = { connect, disconnect, cleanupDatabase }
