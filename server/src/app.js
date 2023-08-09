a71476c3502f0d127b973804a437dbc026719e30
Lioz Akirav <lioz.akirav@gmail.com>
2023-08-09 03:58:28 +0300

    add eslint

--- server/src/app.js
+++ server/src/app.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const { connect } = require('./config/database')
-const logger = require('./config/logger')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const { connect } = require('./config/database');
+const logger = require('./config/logger');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Middleware
-app.set('view engine', 'ejs')
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to the database
-connect(process.env.MONGODB_URI)
+connect(process.env.MONGODB_URI);
 
 // Routes
 
 // Start the server
 const server = app.listen(port, () => {
-  logger.info(`Server started on http://localhost:${port}`)
-})
+    logger.info(`Server started on http://localhost:${port}`);
+});
 
-module.exports = { app, server }
+module.exports = { app, server };
